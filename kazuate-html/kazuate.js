295d407a59943b444e7692023febd12dea3d6676
G284912022 <228491@st.takushoku-u.ac.jp>
2023-06-23 02:28:48 +0900

    第3回課題

--- kazuate-html/kazuate.js
+++ kazuate-html/kazuate.js
@@ -3,7 +3,7 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log("答え(デバッグ用): " + kotae);
 
 //入力回数
-let kaisu = 0;
+let kaisu = 1;
 
 //予想を4回
 //将来以下の呼び出しを削除する
@@ -16,9 +16,10 @@ hantei();
 //ボタンを押した後
 function hantei() {
     let yoso = 4;
-    console.log("1回目の予想: " + yoso);
+    kaisu++;
+    console.log(kaisu + "回目の予想: " + yoso);
     if(yoso === kotae) {
-        if(yoso >= 4) {
+        if(kaisu >= 4) {
             console.log("答えは" + kotae + "でした。すでにゲームは終わっています");
         } else {
             console.log("正解です。おめでとう！");
@@ -26,28 +27,12 @@ function hantei() {
     } else {
         if(kaisu === 3) {
             console.log("まちがい、残念でした答えは" + kotae + "です。");
+        } else if(kaisu >=4) {
+            console.log("答えは" + kotae + "でした。すでにゲームは終わっています");
         } else if(kotae > yoso) {
-        console.log("まちがい、答えはもっと大きいですよ");
-    } else {
+            console.log("まちがい、答えはもっと大きいですよ");
+        } else {
         console.log("まちがい、答えはもっと小さいですよ");
-    }
-}
-
-    //本来は指定された数値
-}
-console.log("1回目の予想: " + yoso);
-if(yoso === kotae) {
-    if(yoso >= 4) {
-        console.log("答えは" + kotae + "でした。すでにゲームは終わっています");
-    } else {
-        console.log("正解です。おめでとう！");
-    }
-} else {
-    if(kaisu === 3) {
-        console.log("まちがい、残念でした答えは" + kotae + "です。");
-    } else if(kotae > yoso) {
-        console.log("まちがい、答えはもっと大きいですよ");
-    } else {
-        console.log("まちがい、答えはもっと小さいですよ");
+        }
     }
 }
