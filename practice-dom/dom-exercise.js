098ad351130c17293fd3a112e4b7227bb33b3d9f
G284912022 <228491@st.takushoku-u.ac.jp>
2023-06-29 15:01:58 +0900

    第4回

--- practice-dom/dom-exercise.js
+++ practice-dom/dom-exercise.js
@@ -17,6 +17,7 @@ let data = [
 let h2 = document.querySelector('h2#ex42');	
 let p = document.createElement('p');
 p.style.textemphasis='sesame green';
+p.textContent='写真表と都市の緯度経度のページです';
 h2.insertAdjacentElement('afterend', p);
 // 練習4-3 写真表作成プログラム
 
