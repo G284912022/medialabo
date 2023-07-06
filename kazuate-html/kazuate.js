9f063b1d6a6afa0a7b707045a40ce5bb881e5718
G284912022 <228491@st.takushoku-u.ac.jp>
2023-07-06 16:01:03 +0900

    第5回

--- kazuate-html/kazuate.js
+++ kazuate-html/kazuate.js
@@ -3,40 +3,45 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log("答え(デバッグ用): " + kotae);
 
 //入力回数
-let kaisu = document.querySelector('span#kaisu');
-kaisu = 1;
+let kaisu = 0;
 
 //予想を4回
 //将来以下の呼び出しを削除する
 //ボタンを押したらhantei()を呼び出す
-hantei();
-hantei();
-hantei();
-hantei();
 
+let a = document.querySelector('button#hantei');
+a.addEventListener('click', hantei);
 //ボタンを押した後
 function hantei() {
-    kaisu.textContent = console.log(kaisu);
-    let yoso = document.querySelector('span#answer');
-    yoso.textContent = 4;
+    kaisu = kaisu + 1;
+    let kaisu1 = document.querySelector('span#kaisu');
+    kaisu1.textContent = kaisu;
+    let n = document.querySelector('input[name="kaitou"]');
+    let yoso = parseInt(n.value);
     let kekka = document.querySelector('p#result');
+    let answer = document.querySelector('span#answer');
+    answer.textContent = parseInt(n.value);
     if(yoso === kotae) {
         if(kaisu >= 4) {
             kekka.textContent = ("答えは" + kotae + "でした。すでにゲームは終わっています");
+            console.log("答えは" + kotae + "でした。すでにゲームは終わっています");
         } else {
             kekka.textContent = ("正解です。おめでとう！");
+            console.log("正解です。おめでとう！");
         }
     } else {
         if(kaisu === 3) {
             kekka.textContent = ("まちがい、残念でした答えは" + kotae + "です。");
+            console.log("まちがい、残念でした答えは" + kotae + "です。");
         } else if(kaisu >=4) {
             kekka.textContent = ("答えは" + kotae + "でした。すでにゲームは終わっています");
+            console.log("答えは" + kotae + "でした。すでにゲームは終わっています");
         } else if(kotae > yoso) {
             kekka.textContent = ("まちがい、答えはもっと大きいですよ");
+            console.log("まちがい、答えはもっと大きいですよ");
         } else {
             kekka.textContent = ("まちがい、答えはもっと小さいですよ");
-            kaisu.textContent += 1;
+            console.log("まちがい、答えはもっと小さいですよ");
         }
     }
-    kaisu++;
 }
--- weather.js
+++ weather.js
@@ -46,23 +46,37 @@ let data = {
 };
 
 ////////// 課題3-2 ここからプログラムを書こう
-let ido = document.querySelector('div#result');
-ido.textContent = ("緯度は"+data.coord.lon);
-let keido = document.querySelector('div#result');
-keido.textContent = ("経度は"+data.coord.lat);
-let tenki = document.querySelector('div#result');
-for(let n of data.weather) {
-  tenki.textContent = ("天気は"+n.description);
+let a = document.querySelector('button#kensaku');
+a.addEventListener('click', kensaku);
+
+function kensaku(){
+  let ido = document.querySelector('li#ido');
+  ido.textContent = ("緯度は"+data.coord.lon);
+  console.log("緯度は"+data.coord.lon);
+  let keido = document.querySelector('li#keido');
+  keido.textContent = ("経度は"+data.coord.lat);
+  console.log("経度は"+data.coord.lat);
+  let tenki = document.querySelector('li#tenki');
+  for(let n of data.weather) {
+    tenki.textContent = ("天気は"+n.description);
+    console.log("天気は"+n.description);
+  }
+  let saitei = document.querySelector('li#saitei');
+  saitei.textContent = ("最低気温:"+data.main.temp_min);
+  console.log("最低気温:"+data.main.temp_min);
+  let saikou = document.querySelector('li#saikou');
+  saikou.textContent = ("最高気温:"+data.main.temp_max);
+  console.log("最高気温:"+data.main.temp_max);
+  let sitsudo = document.querySelector('li#sitsudo');
+  sitsudo.textContent = ("湿度:"+data.main.humidity);
+  console.log("湿度:"+data.main.humidity);
+  let fuusoku = document.querySelector('li#fuusoku');
+  fuusoku.textContent = ("風速:"+data.wind.speed);
+  console.log("風速:"+data.wind.speed);
+  let fuukou = document.querySelector('li#fuukou');
+  fuukou.textContent = ("風向:"+data.wind.deg);
+  console.log("風向:"+data.wind.deg);
+  let toshi = document.querySelector('li#toshi');
+  toshi.textContent = ("都市名は"+data.name);
+  console.log("都市名は"+data.name);
 }
-let saitei = document.querySelector('div#result');
-saitei.textContent = ("最低気温:"+data.main.temp_min);
-let saikou = document.querySelector('div#result');
-saikou.textContent = ("最高気温:"+data.main.temp_max);
-let sitsudo = document.querySelector('div#result');
-sitsudo.textContent = ("湿度:"+data.main.humidity);
-let fuusoku = document.querySelector('div#result');
-fuusoku.textContent = ("風速:"+data.wind.speed);
-let fuukou = document.querySelector('div#result');
-fuukou.textContent = ("風向:"+data.wind.deg);
-let toshi = document.querySelector('div#result');
-toshi.textContent = ("都市名は"+data.name);
